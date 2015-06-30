6ea842aa76613d5ee4634b2bb8c0333f7c1fdf2f
蔚宇 <weiyu.twy@alibaba-inc.com>
2015-06-30 22:22:57 +0800

    removed redundant files

--- Demo/DynamicDemo/Pods/Target Support Files/Pods-DynamicDemo-NVHTarGzip/Pods-DynamicDemo-NVHTarGzip-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DynamicDemo_NVHTarGzip : NSObject
-@end
-@implementation PodsDummy_Pods_DynamicDemo_NVHTarGzip
-@end
--- Demo/DynamicDemo/Pods/Target Support Files/Pods-DynamicDemo/Pods-DynamicDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DynamicDemo : NSObject
-@end
-@implementation PodsDummy_Pods_DynamicDemo
-@end
